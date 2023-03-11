afe021d4f0bd79a650c9ce8efd9efd396147aaeb
admin <admin@Vipul-PC>
2023-03-11 08:07:39 +0530

    i pass second class commit

---
+++ FCP/src/main/java/main/second.java
@@ -0,0 +1,18 @@
+package main;
+
+public class second {
+public static void main(String[] args) {
+		
+		int a=50;
+		int b =100;
+		int res =a+b;
+		System.out.println(res);
+		
+		
+
+		int c=20;
+		int d =30;
+		int rest =c-d;
+		System.out.println(rest);
+	}
+}
